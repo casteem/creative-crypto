faf4f772f8666535e1807041bf2afcecdba8803a
therealFlauwy <33763122+therealFlauwy@users.noreply.github.com>
2018-06-01 15:35:27 -0600

    Updated About content
    switched topics from "Idea" to "Community"

--- src/components/about.js
+++ src/components/about.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import MailchimpSubscribe from 'react-mailchimp-subscribe'
 
 import logo from '../img/about/180407 The Creative Crypto Logo.svg'
 import tribesteemup from '../img/about/pic-tribesteemup.png'
 import kenny from '../img/about/pic-kennyskitchen.png'
+
 
 
 import './about.css'
@@ -12,7 +12,7 @@ export class About extends React.Component {
   render () {
     const portraitInfo = [{
       portrait: tribesteemup,
-      name: '@tribeS#steemup',
+      name: '@TribeSteemUp',
       title: 'Developed by Kennyskitchen'
     }, {
       portrait: kenny,
@@ -32,9 +32,6 @@ export class About extends React.Component {
       )
     })
 
-    const url = 'https://thecreativecrypto.us17.list-manage.com/subscribe/post?u=d8283fcff4083b6d35965e1b3&amp;id=b53263b57e'
-    const SimpleForm = () => <MailchimpSubscribe url={url}/>
-
 
     return (
         <div className="about">
@@ -44,17 +41,20 @@ export class About extends React.Component {
               <p>@TribeSteemUp is a collective voting bot, created to support content & content creators 
 		 focused on making the world a more peaceful & free place. Most of its role in this steem-ciety 
 		 is simply to curate, to help the Steem block-chain recognize & reward the value of great content. 
-		 The focus is on high-quality content, from under-recognized content creators, and more specifically 
-		 on those who create content related to:
-
-		 | Anarchy | Non-Violence | Philosophy | Veganism | Mindfulness |
-		 | Community Empowerment | Love | Original Music | Esotericism |
-		 | Healthy Recipes | Psychedelics | Truth | Permaculture |</p>
+		 The focus is on high-quality content, from under-recognized content creators.
+	      </p>
             </div>
             <div className="team">
               <h2>Community</h2>
               <p>The <strong>@TribeSteemUp</strong> community is build upon an invite-only policy in order to keep
 		 the growth organic and select only new members that fit into the mindset of the group.
+
+		 We support content from the following topics:
+
+
+		 Anarchy | Non-Violence | Philosophy | Veganism | Mindfulness |
+		 Community Empowerment | Love | Original Music | Esotericism |
+		 Healthy Recipes | Psychedelics | Truth | Permaculture
 		</p>
             </div>
           </div>

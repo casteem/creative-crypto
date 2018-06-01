cafa47ed36e7fe589ab6acfd400c8f68386eced1
therealFlauwy <33763122+therealFlauwy@users.noreply.github.com>
2018-06-01 10:46:14 -0600

    About Page
    Created new about page
    New text, new images

--- src/components/about.js
+++ src/components/about.js
@@ -1,62 +1,21 @@
-import React from 'react'
-import MailchimpSubscribe from 'react-mailchimp-subscribe'
 
 import logo from '../img/about/180407 The Creative Crypto Logo.svg'
-import sndbox from '../img/about/180513 CC Sndbox.png'
-import michael from '../img/about/180425 CC Team - Michael.png'
-import kirk from '../img/about/180425 CC Team - Kirk.png'
-import krMarketing from '../img/about/180425 CC Kr-marketing.png'
-import jeff from '../img/about/180425 CC Team - Jeff.png'
-import zolt from '../img/about/180425 CC Team - Zsolt.png'
-import emre from '../img/about/180425 CC Team - Emre.png'
-import carrot from '../img/about/180425 CC Team - Carrotcake.png'
-import rambai from '../img/about/180425 CC Team - Rambai.png'
-import keepit from '../img/about/keepit.png'
+import tribesteemup from '../img/about/pic-tribesteemup.png'
+import kenny from '../img/about/pic-kennyskitchen.png'
+
 
 import './about.css'
 
 export class About extends React.Component {
   render () {
     const portraitInfo = [{
-      portrait: sndbox,
-      name: 'Sndbox',
-      title: 'Developed by the Sndbox Incubator'
+      portrait: tribesteemup,
+      name: 'TribeSteemUp',
+      title: 'Developed by Kennyskitchen'
     }, {
-      portrait: michael,
-      name: 'Michael Lee',
-      title: 'Director of Operations'
-    }, {
-      portrait: kirk,
-      name: 'Kirk Finkel',
-      title: 'Creative Director'
-    }, {
-      portrait: krMarketing,
-      name: '@kr-marketing',
-      title: 'Korean Marketing & Design Team'
-    }, {
-      portrait: jeff,
-      name: '@jeffbernst',
-      title: 'Lead Developer'
-    }, {
-      portrait: zolt,
-      name: '@zolt.vidak',
-      title: 'Lead Illustrator'
-    }, {
-      portrait: emre,
-      name: '@emrebeyler',
-      title: 'Developer'
-    }, {
-      portrait: carrot,
-      name: '@carrotcake',
-      title: 'Illustrator'
-    }, {
-      portrait: rambai,
-      name: '@rambai',
-      title: 'Illustrator'
-    }, {
-      portrait: keepit,
-      name: '@keepit',
-      title: 'Korean Blockchain Journal'
+      portrait: kenny,
+      name: 'Kenny Palurintano',
+      title: 'Founder'
     }
     ]
 
@@ -71,32 +30,27 @@ export class About extends React.Component {
       )
     })
 
-    const url = 'https://thecreativecrypto.us17.list-manage.com/subscribe/post?u=d8283fcff4083b6d35965e1b3&amp;id=b53263b57e'
-    const SimpleForm = () => <MailchimpSubscribe url={url}/>
 
     return (
         <div className="about">
-          <div className="subscribe-container">
-            <div className="subscribe-message">Subscribe to our Weekly Newsletter!</div>
-            <div className="subscribe-form">
-              <SimpleForm/>
-            </div>
-          </div>
           <div className="mission-team">
             <div className="mission">
-              <h2>Mission</h2>
-              <p><strong>The Creative Crypto</strong> is a Steem-powered magazine dedicated to all things creative on
-                the
-                blockchain.</p>
-              <p>We are a news source for thoughtful, approachable and impactful stories surrounding art, design, and
-                innovation in the cryptocurrency landscape.</p>
+              <h2>Idea</h2>
+              <p>@TribeSteemUp is a collective voting bot, created to support content & content creators 
+		 focused on making the world a more peaceful & free place. Most of its role in this steem-ciety 
+		 is simply to curate, to help the Steem block-chain recognize & reward the value of great content. 
+		 The focus is on high-quality content, from under-recognized content creators, and more specifically 
+		 on those who create content related to:
+
+		 | Anarchy | Non-Violence | Philosophy | Veganism | Mindfulness |
+		 | Community Empowerment | Love | Original Music | Esotericism |
+		 | Healthy Recipes | Psychedelics | Truth | Permaculture |</p>
             </div>
             <div className="team">
-              <h2>Team and Contributors</h2>
-              <p>The <strong>@creativecrypto</strong> is changing the way we think about cryptocurrency. Through a
-                global
-                and interdisciplinary team of editors, curators, developers and artists - we reward content creators and
-                readers.</p>
+              <h2>Community</h2>
+              <p>The <strong>@TribeSteemUp</strong> community is build upon an invite-only policy in order to keep
+		 the growth organic and select only new members that fit into the mindset of the group.
+		</p>
             </div>
           </div>
           <div className="about-hr">
--- src/components/tag-list.js
+++ src/components/tag-list.js
@@ -7,6 +7,7 @@ export function TagList() {
 	return (
 		<section className="top-tag-list">
       <NavLink exact to={'/'} className="top-tag top-tag-1" activeClassName="nav-selected">HOME</NavLink>
+      <NavLink to={'/about'} className="top-tag top-tag-2" activeClassName="nav-selected">ABOUT</NavLink>
 		</section>
 	)
 }
